216dcb4fe3adfeaf6964a1e3725cd9a7687d1198
mahmoud aboelmagd <77899534+aboelmagd1@users.noreply.github.com>
2023-11-10 02:11:56 +0300

    change attachment

---
+++ script/script.py
@@ -0,0 +1,16 @@
+﻿import arcpy
+from arcpy import da
+import os
+
+inTable = arcpy.GetParameterAsText(0)
+fileLocation = arcpy.GetParameterAsText(1)
+startName = arcpy.GetParameterAsText(2)
+
+with da.SearchCursor(inTable, ['DATA', 'ATT_NAME', 'ATTACHMENTID']) as cursor:
+    for item in cursor:
+        attachment = item[0]
+        filename = startName + "_"  + str(item[2]) + str(item[1])
+        open(fileLocation + os.sep + filename, 'wb').write(attachment.tobytes())
+        del item
+        del filename
+        del attachment
